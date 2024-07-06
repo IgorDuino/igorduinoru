7ba602d7df5a643e6f9b65a22c32d55640080a9f
Igor Kuzmenkov <igor.kuzmenkov.05@mail.ru>
2024-07-06 23:01:32 +0300

    fix: svelte FontAwesome icons

--- src/lib/components/Whoami.svelte
+++ src/lib/components/Whoami.svelte
@@ -22,9 +22,8 @@
 </section>
 
 <style>
-  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css');
   @import url('/global.css');
-
+  
   .whoami {
     display: flex;
     flex-direction: row;
